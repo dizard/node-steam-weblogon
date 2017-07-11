0ff4883737433bba682f97e27158c13a9e194755
User <mm@csgo.com>
2017-07-11 22:06:04 +0300

    add proxy

--- index.js
+++ index.js
@@ -1,52 +1,56 @@
 var crypto = require('crypto');
 
-var getInterface = require('steam-web-api');
+var request = require('request');
 var SteamCrypto = require('steam-crypto');
+
 
 var SteamEResultOK = 1;
 
-function handleLogOnResponse (logOnResponse) {
-  if (logOnResponse.eresult === SteamEResultOK) {
-    this._webLoginKey = logOnResponse.webapi_authenticate_user_nonce;
-  }
+function handleLogOnResponse(logOnResponse) {
+    if (logOnResponse.eresult === SteamEResultOK) {
+        this._webLoginKey = logOnResponse.webapi_authenticate_user_nonce;
+    }
 }
 
-function SteamWebLogOn (steamClient, steamUser) {
-  this._steamClient = steamClient;
-  this._steamUser = steamUser;
+function SteamWebLogOn(steamClient, steamUser) {
+    this._steamClient = steamClient;
+    this._steamUser = steamUser;
 
-  this._steamClient.on('logOnResponse', handleLogOnResponse.bind(this));
+    this._steamClient.on('logOnResponse', handleLogOnResponse.bind(this));
 }
 
 SteamWebLogOn.prototype.webLogOn = function (callback, proxy) {
-  var sessionKey = SteamCrypto.generateSessionKey();
+    var sessionKey = SteamCrypto.generateSessionKey();
 
-  getInterface('ISteamUserAuth', null, proxy).post('AuthenticateUser', 1, {
-    steamid: this._steamClient.steamID,
-    sessionkey: sessionKey.encrypted,
-    encrypted_loginkey: SteamCrypto.symmetricEncrypt(
-      new Buffer(this._webLoginKey),
-      sessionKey.plain
-    )
-  }, function (statusCode, body) {
-    if (statusCode !== 200) {
-      // request a new login key first
-      this._steamUser.requestWebAPIAuthenticateUserNonce(function (nonce) {
-        this._webLoginKey = nonce.webapi_authenticate_user_nonce;
-        this.webLogOn(callback, proxy);
-      }.bind(this));
-      return;
-    }
+    request.post({
+        'url':'https://api.steampowered.com/ISteamUserAuth/AuthenticateUser/v1',
+        'form' : {
+            steamid: this._steamClient.steamID,
+            sessionkey: sessionKey.encrypted,
+            encrypted_loginkey: SteamCrypto.symmetricEncrypt(
+                new Buffer(this._webLoginKey),
+                sessionKey.plain
+            )
+        },
+        'proxy':proxy
+    }, function (error, response, body) {
+        if (error || response.statusCode !== 200) {
+            this._steamUser.requestWebAPIAuthenticateUserNonce(function (nonce) {
+                this._webLoginKey = nonce.webapi_authenticate_user_nonce;
+                this.webLogOn(callback, proxy);
+            }.bind(this));
+            return;
+        }
 
-    this.sessionID = crypto.randomBytes(12).toString('hex');
-    this.cookies = [
-      'sessionid=' + this.sessionID,
-      'steamLogin=' + body.authenticateuser.token,
-      'steamLoginSecure=' + body.authenticateuser.tokensecure
-    ];
+        this.sessionID = crypto.randomBytes(12).toString('hex');
+        this.cookies = [
+            'sessionid=' + this.sessionID,
+            'steamLogin=' + body.authenticateuser.token,
+            'steamLoginSecure=' + body.authenticateuser.tokensecure
+        ];
 
-    callback(this.sessionID, this.cookies);
-  }.bind(this));
+        callback(this.sessionID, this.cookies);
+    });
 };
 
 module.exports = SteamWebLogOn;
